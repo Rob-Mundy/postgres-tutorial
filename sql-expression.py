b0a8844e2ed971b8e5e87f738e11ec7b2b3dc4ef
Rob-Mundy <robertemundy@googlemail.com>
2023-02-07 16:42:06 +0000

    Add sql-crud.py

--- sql-expression.py
+++ sql-expression.py
@@ -43,7 +43,7 @@ with db.connect() as connection:
     # select_query = artist_table.select()
 
     # Query 2 - select only the "Name" column from the "Artist" table
-    select_query = artist_table.select().with_only_columns([artist_table.c.Name])
+    # select_query = artist_table.select().with_only_columns([artist_table.c.Name])
 
     # Query 3 - select only 'Queen' from the "Artist" table
     # select_query = artist_table.select().where(artist_table.c.Name == "Queen")
@@ -55,7 +55,7 @@ with db.connect() as connection:
     # select_query = album_table.select().where(album_table.c.ArtistId == 51)
 
     # Query 6 - select all tracks where the composer is 'Queen' from the "Track" table
-    # select_query = track_table.select().where(track_table.c.Composer == "Queen")
+    select_query = track_table.select().where(track_table.c.Composer == "Queen")
 
     results = connection.execute(select_query)
     for result in results:
